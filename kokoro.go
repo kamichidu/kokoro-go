faeee551c7e15cbab3384369c8a10781850d6f0b
kamichidu <c.kamunagi@gmail.com>
2017-05-23 11:22:16 +0900

    draft websocket support

--- kokoro.go
+++ kokoro.go
@@ -12,6 +12,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"reflect"
 	"strings"
 	"sync"
 	"time"
@@ -34,19 +35,21 @@ type kokoro struct {
 }
 
 type requestMessage struct {
-	JSONRPC string         `json:"jsonrpc"`
-	Id      interface{}    `json:"id"`
-	Method  string         `json:"method"`
-	Params  *requestParams `json:"params"`
+	JSONRPC string      `json:"jsonrpc"`
+	Id      interface{} `json:"id"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
 }
 
-type requestParams struct {
+type httpRequestParams struct {
 	Type    string            `json:"type"`
 	Url     string            `json:"url"`
 	Headers map[string]string `json:"headers"`
 	Data    interface{}       `json:"data"`
 	Timeout time.Duration     `json:"timeout"`
 }
+
+type websocketRequestParams interface{}
 
 type response struct {
 	JSONRPC string      `json:"jsonrpc"`
@@ -96,29 +99,52 @@ func (self *kokoro) Start(c context.Context, r io.Reader) error {
 				continue
 			}
 
+			rawParams, err := json.Marshal(msg.Params)
+			if err != nil {
+				self.Logger.Errorf("Can't marshal json: %s", err)
+				continue
+			}
+
 			var (
 				successData interface{}
 				errorData   *errorObject
 			)
 			switch msg.Method {
 			case "http":
+				params, err := self.UnmarshalRequestParamsForREST(rawParams)
+				if err != nil {
+					self.Logger.Errorf("Can't unmarshal params: %s", err)
+					continue
+				}
+				msg.Params = params
 				if successData, errorData, err = self.TranslateToREST(msg); err != nil {
-					self.Logger.Errorf("Error: %s", err)
+					self.Logger.Errorf("REST API Error: %s", err)
 				}
 			case "websocket":
-				self.Logger.Warn("Sorry, not implemented yet")
+				params, err := self.UnmarshalRequestParamsForWebSocket(rawParams)
+				if err != nil {
+					self.Logger.Errorf("Can't unmarshal params: %s", err)
+					continue
+				}
+				msg.Params = params
+				if successData, errorData, err = self.TranslateToWebSocket(msg); err != nil {
+					self.Logger.Errorf("WebSocket Error: %s", err)
+				}
 			}
-			reply := &response{
-				JSONRPC: msg.JSONRPC,
-				Id:      msg.Id,
-			}
-			if errorData != nil {
-				reply.Error = errorData
-			} else {
-				reply.Result = successData
-			}
-			if err = self.WriteJSON(reply); err != nil {
-				self.Logger.Errorf("Can't write output: %s", err)
+			// no reply when result and error are empty
+			if successData != nil || errorData != nil {
+				reply := &response{
+					JSONRPC: msg.JSONRPC,
+					Id:      msg.Id,
+				}
+				if errorData != nil {
+					reply.Error = errorData
+				} else {
+					reply.Result = successData
+				}
+				if err = self.WriteJSON(reply); err != nil {
+					self.Logger.Errorf("Can't write output: %s", err)
+				}
 			}
 		}
 	}(bufio.NewReader(r))
@@ -177,7 +203,27 @@ func (self *kokoro) startWS(c context.Context) error {
 				self.Logger.Errorf("Can't read websocket: %s", err)
 				continue
 			}
-			self.Logger.Infof("websocket = %v", string(b))
+			self.Logger.Debugf("websocket = %v", string(b))
+			data := make(map[string]interface{})
+			if err = json.Unmarshal(b, &data); err != nil {
+				self.Logger.Errorf("Can't unmarshal websocket message: %s", err)
+				continue
+			}
+			switch s := data["type"].(type) {
+			case string:
+				if s == "ping" {
+					self.Logger.Debugf("Ignore ping message: %v", data)
+					continue
+				}
+			}
+			notif := &response{
+				JSONRPC: "2.0",
+				Id:      "__websocket__",
+				Result:  data,
+			}
+			if err = self.WriteJSON(notif); err != nil {
+				self.Logger.Errorf("Can't write websocket output: %s", err)
+			}
 		}
 	}()
 	select {
@@ -195,15 +241,24 @@ func (self *kokoro) WriteJSON(v interface{}) error {
 	return json.NewEncoder(self.w).Encode(v)
 }
 
+func (self *kokoro) UnmarshalRequestParamsForREST(b []byte) (interface{}, error) {
+	params := new(httpRequestParams)
+	return params, json.Unmarshal(b, params)
+}
+
 func (self *kokoro) TranslateToREST(msg *requestMessage) (interface{}, *errorObject, error) {
 	client := new(http.Client)
+	params, ok := msg.Params.(*httpRequestParams)
+	if !ok {
+		self.Logger.Panicf("Type mismatch: wants %v, but %v", reflect.TypeOf(new(httpRequestParams)), reflect.TypeOf(msg.Params))
+	}
 
-	requestMethod := strings.ToUpper(msg.Params.Type)
+	requestMethod := strings.ToUpper(params.Type)
 	if requestMethod == "" {
 		requestMethod = http.MethodGet
 	}
 
-	requestUrl, err := url.ParseRequestURI(msg.Params.Url)
+	requestUrl, err := url.ParseRequestURI(params.Url)
 	if err != nil {
 		return nil, nil, fmt.Errorf("Can't parse request url: %s", err)
 	}
@@ -219,13 +274,13 @@ func (self *kokoro) TranslateToREST(msg *requestMessage) (interface{}, *errorObj
 	}
 	var requestBody io.Reader
 	if requestMethod == "POST" {
-		data, err := json.Marshal(msg.Params.Data)
+		data, err := json.Marshal(params.Data)
 		if err != nil {
 			return nil, nil, fmt.Errorf("Can't create request payload: %s", err)
 		}
 		requestBody = bytes.NewReader(data)
 	} else {
-		if queryParams, ok := msg.Params.Data.(map[string]interface{}); ok {
+		if queryParams, ok := params.Data.(map[string]interface{}); ok {
 			for k, v := range queryParams {
 				requestUrl.Query().Set(k, fmt.Sprintf("%v", v))
 			}
@@ -236,14 +291,15 @@ func (self *kokoro) TranslateToREST(msg *requestMessage) (interface{}, *errorObj
 	if err != nil {
 		return nil, nil, fmt.Errorf("Can't create REST API Request: %s", err)
 	}
-	if msg.Params.Headers != nil {
-		for k, v := range msg.Params.Headers {
+	if params.Headers != nil {
+		for k, v := range params.Headers {
 			req.Header.Set(k, v)
 		}
 	}
 	req.Header.Set("X-Access-Token", self.AccessToken)
 
-	client.Timeout = msg.Params.Timeout
+	self.Logger.Debugf("Send http request with timeout %v: %v", params.Timeout, req)
+	client.Timeout = params.Timeout
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, nil, fmt.Errorf("Error: %s", err)
@@ -272,4 +328,31 @@ func (self *kokoro) TranslateToREST(msg *requestMessage) (interface{}, *errorObj
 		}
 	}
 	return successData, errorData, nil
+}
+
+func (self *kokoro) UnmarshalRequestParamsForWebSocket(b []byte) (interface{}, error) {
+	params := new(websocketRequestParams)
+	return params, json.Unmarshal(b, params)
+}
+
+func (self *kokoro) TranslateToWebSocket(msg *requestMessage) (interface{}, *errorObject, error) {
+	if self.wsConn == nil {
+		self.Logger.Panicf("WebSocket connection is nil")
+	}
+	params, ok := msg.Params.(websocketRequestParams)
+	if !ok {
+		self.Logger.Panicf("Type mismatch: wants %v, but %v", reflect.TypeOf(new(websocketRequestParams)), reflect.TypeOf(msg.Params))
+	}
+
+	w, err := self.wsConn.NextWriter(websocket.TextMessage)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Failed to create ws writer: %s", err)
+	}
+	defer w.Close()
+
+	self.Logger.Debugf("Send data via websocket: %v", params)
+	if err = json.NewEncoder(w).Encode(params); err != nil {
+		return nil, nil, fmt.Errorf("Can't marshal json: %s", err)
+	}
+	return nil, nil, nil
 }
