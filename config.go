99b9d1414e7cccd3c3c71518a54381d3fc6c5c35
kamichidu <c.kamunagi@gmail.com>
2017-11-02 14:46:32 +0900

    kokoro-go config

--- config.go
+++ config.go
@@ -1,19 +1,137 @@
 package main
 
-// Application configuration
+import (
+	"encoding/json"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/urfave/cli.v1"
+	"io"
+	"os"
+	"path/filepath"
+	"reflect"
+	"runtime"
+)
+
+var defaultConfigFilename string
+
+func init() {
+	const filename = "kokoro-go/config.json"
+
+	if runtime.GOOS == "windows" {
+		defaultConfigFilename = filepath.Join(os.Getenv("AppData"), filename)
+	} else {
+		defaultConfigFilename = filepath.Join(os.Getenv("HOME"), filename)
+	}
+}
+
 type appConfig struct {
-	// kokoro-io hostname, "host:port" or "host"
-	Host string
+	Token string `json:"token"`
+}
 
-	// Use http instead of https, and ws of wss
-	Insecure bool
+func (self *appConfig) WriteFile(filename string) error {
+	if err := os.MkdirAll(filepath.Dir(filename), 0755); err != nil {
+		return err
+	}
 
-	// Access token
-	AccessToken string
+	wc, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer wc.Close()
 
-	// The level for log
-	LogLevel string
+	return json.NewEncoder(wc).Encode(self)
+}
 
-	// The filename for log output
-	LogFile string
+func newAppConfigFrom(filename string) (*appConfig, error) {
+	log.Debugf("loading config from %s", filename)
+	v := &appConfig{}
+	rc, err := os.Open(filename)
+	if os.IsNotExist(err) {
+		log.Debugf("config file is not exists (%s)", filename)
+		return v, nil
+	} else if err != nil {
+		log.Debugf("can't open file (%s): %s", filename, err)
+		return nil, err
+	}
+	defer rc.Close()
+
+	return v, json.NewDecoder(rc).Decode(v)
+}
+
+type cmdConfig struct{}
+
+func (self *cmdConfig) Action(c *cli.Context) error {
+	configFilename := c.GlobalString("c")
+	config, err := newAppConfigFrom(configFilename)
+	if err != nil {
+		return err
+	}
+
+	switch c.NArg() {
+	case 0:
+		return self.list(c.App.Writer, config)
+	case 1:
+		return self.get(c.App.Writer, config, c.Args().First())
+	case 2:
+		if err := self.set(config, c.Args().Get(0), c.Args().Get(1)); err != nil {
+			return err
+		}
+
+		return config.WriteFile(configFilename)
+	default:
+		return cli.NewExitError("See help", 128)
+	}
+}
+
+func (self *cmdConfig) list(w io.Writer, config *appConfig) error {
+	typ := reflect.TypeOf(config).Elem()
+	for i := 0; i < typ.NumField(); i++ {
+		f := typ.Field(i)
+		name := f.Tag.Get("json")
+		if err := self.get(w, config, name); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (self *cmdConfig) get(w io.Writer, config *appConfig, name string) error {
+	rv := reflect.ValueOf(config).Elem()
+	typ := rv.Type()
+	for i := 0; i < typ.NumField(); i++ {
+		ftyp := typ.Field(i)
+		if ftyp.Tag.Get("json") == name {
+			fv := rv.Field(i)
+			fmt.Fprintf(w, "%s=%v\n", name, fv.Interface())
+			return nil
+		}
+	}
+	return cli.NewExitError(fmt.Sprintf("No such config: %s", name), 128)
+}
+
+func (self *cmdConfig) set(config *appConfig, name string, value string) error {
+	rv := reflect.ValueOf(config).Elem()
+	typ := rv.Type()
+	for i := 0; i < typ.NumField(); i++ {
+		ftyp := typ.Field(i)
+		if ftyp.Tag.Get("json") == name {
+			fv := rv.Field(i)
+			switch fv.Kind() {
+			case reflect.String:
+				fv.SetString(value)
+			default:
+				log.Panicf("no implementation for %s", fv.Kind())
+			}
+			return nil
+		}
+	}
+	return cli.NewExitError(fmt.Sprintf("No such config: %s", name), 128)
+}
+
+func init() {
+	cmd := &cmdConfig{}
+	commands = append(commands, cli.Command{
+		Name:   "config",
+		Action: cmd.Action,
+	})
 }
--- main.go
+++ main.go
@@ -1,131 +1,84 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"flag"
 	"fmt"
-	"github.com/gorilla/websocket"
-	log "github.com/sirupsen/logrus"
 	"io"
 	"os"
 	"path/filepath"
-	"sync"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/urfave/cli.v1"
 )
 
 var appVersion string
 
-type wsReader struct {
-	*websocket.Conn
-}
+var commands = []cli.Command{}
 
-func (self *wsReader) Read(p []byte) (int, error) {
-	mt, b, err := self.Conn.ReadMessage()
-	if err != nil {
-		return len(b), err
+func initLogger(c *cli.Context) error {
+	l := log.StandardLogger()
+	if lvl, err := log.ParseLevel(c.String("loglevel")); err != nil {
+		return cli.NewExitError(err, 128)
+	} else {
+		l.Level = lvl
 	}
-	if mt != websocket.TextMessage {
-		return 0, errors.New("Only supported TextMessage")
+	if filename := c.String("logfile"); filename != "-" {
+		filename = filepath.Clean(filepath.FromSlash(filename))
+
+		if info, err := os.Stat(filepath.Dir(filename)); err != nil {
+			// Output directory is not exists, create it
+			if err := os.MkdirAll(filepath.Dir(filename), 0755); err != nil {
+				return err
+			}
+		} else if !info.IsDir() {
+			return cli.NewExitError(fmt.Sprintf("Invalid logfile: %s", filename), 128)
+		}
+
+		wc, err := os.Create(filename)
+		if err != nil {
+			return err
+		}
+		l.Out = wc
 	}
-	return copy(p, b), nil
+	return nil
 }
-
-var _ io.Reader = (*wsReader)(nil)
-
-type wsWriter struct {
-	*websocket.Conn
-}
-
-func (self *wsWriter) Write(data []byte) (int, error) {
-	return len(data), self.Conn.WriteMessage(websocket.TextMessage, data)
-}
-
-var _ io.Writer = (*wsWriter)(nil)
 
 func run(in io.Reader, out io.Writer, errOut io.Writer, args []string) int {
-	// parse flags
-	var (
-		showVersion bool
-		config      = new(appConfig)
-	)
-	flags := flag.NewFlagSet("kokoro-go", flag.ExitOnError)
-	flags.BoolVar(&showVersion, "v", false, "Show version")
-	flags.StringVar(&config.Host, "H", "kokoro.io", "`HOST` for REST API/WebSocket API")
-	flags.BoolVar(&config.Insecure, "k", false, "Use insecure connection")
-	flags.StringVar(&config.LogLevel, "l", "info", "Log `LEVEL`")
-	flags.StringVar(&config.LogFile, "f", "-", "Log `FILE`")
-	flags.Usage = func() {
-		fmt.Fprintf(errOut, "Usage of %s:\n\n", filepath.Base(args[0]))
-		fmt.Fprintf(errOut, "  %s [{options}...] {access token}\n\n", filepath.Base(args[0]))
-		flags.PrintDefaults()
-	}
-	if err := flags.Parse(args[1:]); err != nil {
-		fmt.Fprintf(errOut, "Can't parse arguments: %s\n", err)
-		return 128
-	}
-
-	if showVersion {
-		fmt.Fprintln(out, appVersion)
-		return 0
-	}
-
-	config.AccessToken = flags.Arg(0)
-	if config.AccessToken == "" {
-		flags.Usage()
-		return 128
-	}
-
-	// init logger
+	// init global logger
 	logger := log.StandardLogger()
 	logger.Formatter = &log.TextFormatter{}
-	if config.LogFile == "-" {
-		logger.Out = errOut
-	} else {
-		if err := os.MkdirAll(filepath.Dir(config.LogFile), 0755); err != nil {
-			fmt.Fprintf(errOut, "Can't create directory: %s\n", err)
-			return 128
-		}
-		if fw, err := os.OpenFile(config.LogFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644); err != nil {
-			fmt.Fprintf(errOut, "Can't create logfile: %s\n", err)
-			return 128
-		} else {
-			defer fw.Close()
-			logger.Out = fw
-		}
-	}
-	if lvl, err := log.ParseLevel(config.LogLevel); err != nil {
-		fmt.Fprintf(errOut, "Can't recognize loglevel: %s\n", err)
-		return 128
-	} else {
-		logger.Level = lvl
-	}
+	logger.Out = errOut
 
-	// build app
-	app := &kokoro{
-		mutex:       new(sync.Mutex),
-		w:           out,
-		Insecure:    config.Insecure,
-		Host:        config.Host,
-		AccessToken: config.AccessToken,
-		Logger:      logger,
+	app := cli.NewApp()
+	app.Writer = out
+	app.ErrWriter = errOut
+	app.Name = "kokoro-go"
+	app.Version = appVersion
+	app.Commands = commands
+	app.Before = initLogger
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "loglevel",
+			Value: "info",
+		},
+		&cli.StringFlag{
+			Name:  "logfile",
+			Value: "-",
+		},
+		&cli.StringFlag{
+			Name:  "c",
+			Value: defaultConfigFilename,
+		},
 	}
-	if err := app.Start(context.Background(), in); err != nil {
-		logger.Errorf("Error: %s", err)
-		return 1
+	if err := app.Run(args); err != nil {
+		// err is already reported by app.Run() inside
+		if eerr, ok := err.(*cli.ExitError); ok {
+			return eerr.ExitCode()
+		} else {
+			fmt.Fprintln(errOut, err)
+			return 1
+		}
 	}
 	return 0
-
-	// ws, _, err := websocket.DefaultDialer.Dial(proxyUrl, nil)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer ws.Close()
-	//
-	// go io.Copy(&wsWriter{ws}, r)
-	// _, err = io.Copy(w, &wsReader{ws})
-	// if err != nil {
-	// 	panic(err)
-	// }
 }
 
 func main() {
